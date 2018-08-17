e590045c3f948dc9ec9acc0b792c6dc7821c8fe7
vermasubina <subinaverma7837@gmail.com>
2018-08-17 21:06:03 +0530

    assignment 2

---
+++ assignm.py
@@ -0,0 +1,30 @@
+#Q1.PRINT SOMETHING.
+a="my name is subina"
+print(a)
+
+#Q2.CONCATINATE STRINGS
+a="hey"
+print('hey' + 'you')
+
+#Q3.PRINT 3 USER DEFINED VARIABLES.
+s=int(input("enter value of s"))
+p=int(input("enter value of p"))
+q=int(input("enter value of q"))
+print(s)
+print(p)
+print(q)
+
+#Q4.PRINT LET'S GET STARTED.
+a="let's get started"
+print(a)
+
+#Q5.PRINT THE GIVEN VALUES USING PLACEHOLDERS.
+a="chitkara"
+b="university"
+sal=10000
+print("%s %s sal is %d."%(a,b,sal))
+
+#Q6.FIND THE AREA OF CIRCLE WITH USER DEFINED RADIUS.
+r=int(input("enter the radius"))
+a=3.14*r*r
+print(a)
